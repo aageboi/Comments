f53012f4d323a14446ec40487d66711e8eced48d
Heri Gunawan <aageboi@gmail.com>
2016-01-06 15:20:27 +0700

    - long polling - new design

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/comments', function(req, res){
+app.get('/api/comments/:id/:kanal', function(req, res){
+	var commentAPI = 'https://comment.detik.com/v2/?get';
 	var options = {
-		url: 'https://comment.detik.com/v2/?get&key=3111495&group=72&limit=10&sort=newest&date=06-01-2016&start=1&limit=2'
+		url: commentAPI + '&key='+req.params.id+'&group='+req.params.kanal+'&limit=10&sort=newest&date=06-01-2016&start=1&limit=10'
 	}
 
 	function callback(error, response, body) {
@@ -27,6 +28,9 @@ app.get('/api/comments', function(req, res){
 	request(options, callback);
 });
 
+app.get('/:id/:kanal', function(req, res){
+	res.send(req.query);
+});
 
 app.listen(app.get('port'), function(){
 	console.log('Server started: http://localhost:'+app.get('port')+'/');
--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,25 +1,30 @@
 // tutorial1.js
 var CommentBox = React.createClass({
-	getInitialState: function() {
-		return {entries: []};
-	},
-	componentDidMount: function(){
+	loadCommentsFromServer: function(){
 		$.ajax({
 			url: this.props.url,
 			dataType: 'json',
 			cache: false,
 			success: function(data) {
-				this.setState({entries:data.entries});
+				this.setState({entries:data.entries, total:data.counter, title:data.entries[0].title, link:data.entries[0].link});
 			}.bind(this),
 			error: function(xhr, status, err) {
 				console.error(this.props.url, status, err.toString());
 			}.bind(this)
 		});
 	},
+	getInitialState: function() {
+		return {entries: [], total: 0, title: '', link: ''};
+	},
+	componentDidMount: function(){
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function() {
 		return (
 			<div className="commentBox">
-				<h1>Comments</h1>
+				<h1><a href={this.state.link}>{this.state.title}</a></h1>
+				<h2>{this.state.total} Comments</h2>
 				<CommentList data={this.state.entries} />
 				<CommentForm />
 			</div>
@@ -31,16 +36,16 @@ var CommentList = React.createClass({
 	render: function() {
 		var commentNodes = this.props.data.map(function(comment){
 			return (
-				<Comment author={comment.prf_name} key={comment.com_id}>
+				<Comment key={comment.com_id} data={comment}>
 					{comment.cmt_text}
 				</Comment>
 			);
 		});
 
 		return (
-			<div className="commentList">
+			<ul className="comment_list media-list">
 				{commentNodes}
-			</div>
+			</ul>
 		);
 	}
 });
@@ -63,18 +68,46 @@ var Comment = React.createClass({
 		return { __html : rawMarkup };
 	},
 	render: function() {
+		var twlink = "https://twitter.com/intent/tweet?url=" + this.props.data.link;
 		return (
-			<div className="comment">
-				<h2 className="commentAuthor">
-					{this.props.author}
-				</h2>
-				<span dangerouslySetInnerHTML={this.rawMarkup()} />
-			</div>
+			<li className="list_dk media">
+				<span className="user_pic pull-left">
+					<img src={this.props.data.prf_avatar} />
+				</span>
+				<div className="list_dk_text media-body mb_parent" id={this.props.key}>
+					<span className="dk_name controls controls-row">
+						<b>{this.props.data.prf_name} </b>
+					</span>
+					<span className="dk_name dk_user">@{this.props.data.prf_user}</span> &middot; <span className="dk_date">11 menit yang lalu</span>
+					<div className="dk_komen" dangerouslySetInnerHTML={this.rawMarkup()} />
+					<input className="parent_id" type="hidden" value={this.props.key} />
+					<div className="dk_action reaction">
+						<a className="dk_balas">
+							<span className="count">0</span><span>Balas</span>
+						</a>
+						<a className="dk_fav">
+							<span className="count">0</span><span>Favorit</span>
+						</a>
+						<a className="to_share">
+							<span>Share</span>
+							<span className="share_elm">
+								<a className="selm se_fb"></a> 
+								<a className="selm se_tw" href={twlink} target="__blank"> </a>
+							</span>
+						</a>
+						<a className="dk_flag">
+							<span>Laporkan</span>
+						</a>
+						<div className="clearfix"></div>
+					</div>
+				</div>
+				<div className="clearfix"></div>
+			</li>
 		);
 	}
 });
 
 ReactDOM.render(
-	<CommentBox url='/api/comments' />,
+	<CommentBox url='/api/comments/3111545/10' pollInterval={2000} />,
 	document.getElementById('content')
 );
